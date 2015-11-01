8d9184f4c97e99451480596bd024d3e493252820
Juan Marroquin <juanm95@stanford.edu>
2015-11-01 01:53:10 -0700

    Input checking activated, card only appearing when convert pressed. Going to check if works on mobile.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,8 @@
 $(function() {
+
+    function underSixty(time) {
+        return isNaN(time) || parseInt(time) < 60;
+    }
   $("#convert").click(function () {
     var units = $("#units").get(0).selectedItemLabel;
     console.log($("#distance").get(0));
@@ -7,6 +11,20 @@ $(function() {
     var seconds = parseInt($("#seconds").get(0).value);
     var centi = parseInt($("#centi").get(0).value);
     var multiplier;
+    var error="";
+    if (units === undefined || distance === undefined || minutes === undefined || centi === undefined || seconds === undefined) {
+        error += "Missing field(s)\n";
+    }
+    if (!underSixty(minutes)) {
+        error += "Minutes must be under sixty.\n"
+    }
+    if (!underSixty(seconds)) {
+        error += "Seconds must be under sixty.\n"
+    }
+    if (error !== "") {
+        alert(error);
+        return;
+    }
     if (units === "Yards") {
       multiplier = 1.165;
     } else {
@@ -18,5 +36,6 @@ $(function() {
     seconds -= minutes*60;
     $("#repeatDistance").html(distance.toString() + " " +units);
     $("#result").html(minutes + " minutes and " + seconds.toFixed(2) + " seconds");
+    $("#resultCard").css("visibility", "visible");
   });
 });
