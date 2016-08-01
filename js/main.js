fc5029a0ef2d9fc14f3ea1bf491a64f0c05ed2d5
Juan Marroquin <juam95@stanford.edu>
2016-07-31 23:29:42 -0400

    Redesign first commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
-var submitButton = document.getElementById("calculate");
-var form = document.getElementById("calculator");
-var num1 = document.getElementById("num1");
-var num2 = document.getElementById("num2");
-var answer = document.getElementById("answer")
-submitButton.onclick(calculate);
-function calculate() {
-    answer.innerHTML = num1.value + num2.value
+animationsOn = false;
+
+$(document).ready(function () {
+    if (animationsOn) setArrowLight();
+
+    // The following variables are defined for my animateNavBar function
+    var decoyMenu = $('#jplaceholder');
+    var actualMenu = $('#jactual');
+
+    function animateNavBar() {
+        if (decoyMenuAtTop(decoyMenu)) {
+            actualMenu.show();
+            decoyMenu.addClass('invisible');
+        } else {
+            actualMenu.hide();
+            decoyMenu.removeClass('invisible');
+        }
+    }
+    document.onscroll = animateNavBar;
+
+    var halfWayPoint = $(window).height() / 2;
+    $('body').scrollspy({
+        target: '#sideContentAnchor'
+        , offset: halfWayPoint
+    });
+
+})
+
+function decoyMenuAtTop(decoyMenu) {
+    var fixpoint = decoyMenu.offset().top;
+    return $(window).scrollTop() >= fixpoint;
+}
+
+function setArrowLight() {
+    var fadeTime = 800;
+
+    $('#jscroll').delay(5000).fadeIn(400, function () {
+        setInterval(function () {
+            $('#jscroll').fadeOut(fadeTime);
+            $('#jscroll').fadeIn(fadeTime);
+        }, fadeTime / 2)
+    })
 }
